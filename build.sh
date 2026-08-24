04f5980ce4f774aa5b3c8674bcc5a638eae9b4aa
corvinjs <174349145+corvinjs@users.noreply.github.com>
2026-08-24 22:59:32 +0200

    correctly set envvar

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ export PATH="$GEM_HOME/bin:$PATH"
 gem install jekyll bundler
 bundle install
 
-bundle exec JEKYLL_ENV=production jekyll build --baseurl "$BASE_URL"
+JEKYLL_ENV=production bundle exec jekyll build --baseurl "$BASE_URL"
